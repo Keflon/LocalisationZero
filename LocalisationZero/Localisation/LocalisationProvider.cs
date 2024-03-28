a804ba296221c076971bfea86d8e98999a9d721d
Keith Pickford <Keith@FunctionZero.com>
2024-03-28 15:20:38 +0000

    LocalisationProvider can be created with an IEnumerable<string>.

--- LocalisationZero/Localisation/LocalisationProvider.cs
+++ LocalisationZero/Localisation/LocalisationProvider.cs
@@ -7,16 +7,27 @@
             GetLookup = getLookup;
             LanguageName = languageName;
         }
-        //public LocalisationProvider(Func<LocalisationPack> getLookup, string languageName)
-        //{
-        //    GetLookup = GetLookupFromStringList(getLookup);
-        //    LanguageName = languageName;
-        //}
+        public LocalisationProvider(Func<IEnumerable<string>> getLookup, string languageName)
+        {
+            GetLookup = ()=> GetLocalisationPackFromStringList(getLookup);
+            LanguageName = languageName;
+        }
 
-        //private Func<LocalisationPack> GetLookupFromStringList(Func<LocalisationPack> getLookup)
-        //{
-        //    throw new NotImplementedException();
-        //}
+        private LocalisationPack GetLocalisationPackFromStringList(Func<IEnumerable<string>> getLookup)
+        {
+            // LocalisationPack
+            //    LocalisationRecord
+            //        LocalisationItem
+
+            var localisationItemList = new List<LocalisationItem>();
+
+            foreach (var localString in getLookup())
+                localisationItemList.Add(new LocalisationItem("True", localString));
+
+            var recordList = new List<LocalisationRecord> { new LocalisationRecord(localisationItemList) };
+
+            return new LocalisationPack(recordList);
+        }
 
         public Func<LocalisationPack> GetLookup { get; }
         public string LanguageName { get; }
